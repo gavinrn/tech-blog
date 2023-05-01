2a4badf3be8a0d8db677795d97677667d0e56b63
Gavin McNutt <gavinmcnutt1@gmail.com>
2023-04-30 17:45:44 -0700

    fixed and added basic starting out file structure

---
+++ models/Post.js
@@ -0,0 +1,37 @@
+// Here is where we set up our Dish model, for when we are ready to connect to a database in future activities.
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    guest_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Post',
+  }
+);
+
+module.exports = Post;
